6c49c38337801de66f46561f71c540fcde1b3c5d
yxxhero <526169407@qq.com>
2016-09-20 06:44:19 +0800

    change monitor_server.py

--- monitor_server.py
+++ monitor_server.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python2.7
 from utils import global_setting
 from templet import Basetemplet
+from utils import redishelper
+import time
 client_1=Basetemplet.linuxtemplet()
 client_1.host_ip='192.169.186.134'
 client_2=Basetemplet.linuxtemplet()
-client_3.host_ip='192.169.186.135'
+client_2.host_ip='192.169.186.135'
 client_3=Basetemplet.unixtemplet()
-client_4.host_ip='192.169.186.134'
+client_3.host_ip='192.169.186.134'
 group=['192.169.186.134','192.169.186.135']
+client_grouo=[clietn_1,client_2,clietn_3]
+for ip in group:
+    client_config=ip+':monitor_config'
+    
+    for client in client_group:
+        if ip==client.host_ip:
+
+
+
