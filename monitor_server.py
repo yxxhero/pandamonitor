5e9f810b3d5a4982ef4cb9d10634440a034addef
yxxhero <526169407@qq.com>
2016-09-20 07:20:43 +0800

    fix bug

--- monitor_server.py
+++ monitor_server.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python2.7
+# _*_ coding:utf-8 _*_
 from utils import global_setting
 from templet import Basetemplet
-from utils import redishelper
+#from utils import redishelper
 import time
 client_1=Basetemplet.linuxtemplet()
 client_1.host_ip='192.169.186.134'
@@ -10,12 +11,14 @@ client_2.host_ip='192.169.186.135'
 client_3=Basetemplet.unixtemplet()
 client_3.host_ip='192.169.186.134'
 group=['192.169.186.134','192.169.186.135']
-client_grouo=[clietn_1,client_2,clietn_3]
+client_group=[client_1,client_2,client_3]
 for ip in group:
     client_config=ip+':monitor_config'
-    
+    server_list=[] 
     for client in client_group:
         if ip==client.host_ip:
+            server_list.append(client.server)
+    print server_list[:]
 
 
 
--- templet/Basetemplet.py
+++ templet/Basetemplet.py
@@ -5,10 +5,12 @@ class basetemplet(object):
         self.host_ip=''
         self.group=''
 class linuxtemplet(basetemplet):
-    super(linuxtemplet,self).__init():
+    def __init__(self):
+        super(linuxtemplet,self).__init__()
         self.server=['mem_info']
         self.name='linux'
 class unixtemplet(basetemplet):
-    super(unixtemplet,self).__init():
+    def __init__(self):
+        super(unixtemplet,self).__init__()
         self.server=['cpu_info']
         self.name='unix'
